acb12a54c56ae7510c59b4298a498afe020cc569
RustyNova <rusty.nova.jsb@gmail.com>
2026-05-29 09:37:44 +0200

    chore: update CI to nix

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Rust devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              pkg-config
+
+              # CI / Linting tools
+              cargo-mutants
+              cargo-hack
+              cargo-msrv
+              cargo-audit
+              cargo-machete
+
+              (rust-bin.stable.latest.default.override {
+                extensions = [
+                  "cargo"
+                  "clippy"
+                  "rust-src"
+                  "rust-analyzer"
+                ];
+              })
+            ];
+          };
+      }
+    );
+}
